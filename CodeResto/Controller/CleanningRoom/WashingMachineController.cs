b6ca126263bfb7cf710ec9104a246d259b56ac51
pierre265 <kevinngako@gmail.com>
2022-12-02 11:01:25 +0100

    Classes Kitchen et washing

---
+++ CodeResto/Controller/CleanningRoom/WashingMachineController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterChefUcac
+{
+    /// <summary>
+    /// Classe qui contrôle la machine à laver
+    /// </summary>
+    class WashingMachineController
+    {
+
+    }
+}
